a288f4e2fe2411900e6ba4cf96a4d448659ee0f9
LooLzzz <noaml12@hotmail.com>
2024-06-01 18:39:04 +0000

    empty commit

--- src/types/express.d.ts
+++
@@ -1,7 +0,0 @@
-import { RequestHandler } from "express";
-
-declare module 'express-serve-static-core' {
-    interface Request extends RequestHandler {
-      user?: string | JwtPayload;
-    }
-  }
