98a7b3098228f6ee445125f59b550e5e8fbca390
Daniel Wiederman <danielwiederman@gmail.com>
2024-06-01 21:58:29 +0300

    new string legit validation

--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -1,8 +1,16 @@
 import { NextFunction, Request, RequestHandler, Response } from "express"
 import { ValidationChain, check, validationResult } from "express-validator"
 
+// matches(/^\p{L}+$/u): Ensures the value contains only alphabetic characters from any language.
+// The \p{L} matches any kind of letter from any language, and the u flag enables Unicode matching.
+
 export const mustStringValidator = (fieldName: string) => {
-    return check(fieldName).not().isEmpty().withMessage(`${fieldName} is required`)
+    return check(fieldName)
+    .not()
+    .isEmpty()
+    .isString()
+    .matches(/^\p{L}+$/u)
+    .withMessage(`${fieldName} is required`)
 }
 
 export const passwordValidator = () => {
